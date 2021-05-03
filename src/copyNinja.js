77558776e800213ebf404a4cb71059a8d53a297e
b0829065 <b0829065@cgu.edu.tw>
2021-05-04 01:02:35 +0800

    function<80

--- src/copyNinja.js
+++ src/copyNinja.js
@@ -1,46 +1,98 @@
-import { makeStyles } from '@material-ui/core/styles';
-import Card from '@material-ui/core/Card';
-import CardActionArea from '@material-ui/core/CardActionArea';
-import CardActions from '@material-ui/core/CardActions';
-import CardContent from '@material-ui/core/CardContent';
-import CardMedia from '@material-ui/core/CardMedia';
+import React from 'react';
+import Avatar from '@material-ui/core/Avatar';
 import Button from '@material-ui/core/Button';
+import CssBaseline from '@material-ui/core/CssBaseline';
+import TextField from '@material-ui/core/TextField';
+import FormControlLabel from '@material-ui/core/FormControlLabel';
+import Checkbox from '@material-ui/core/Checkbox';
+import Link from '@material-ui/core/Link';
+import Paper from '@material-ui/core/Paper';
+import Box from '@material-ui/core/Box';
+import Grid from '@material-ui/core/Grid';
+import LockOutlinedIcon from '@material-ui/icons/LockOutlined';
 import Typography from '@material-ui/core/Typography';
+import { makeStyles } from '@material-ui/core/styles';
 
-const useStyles = makeStyles({
-    root: {
-    maxWidth: 345,
-    },
-    media: {
-    height: 140,
-    },
-   });
-   
-   const MultiButton=(num)=>{
+export default function SignInSide() {
     const classes = useStyles();
-    return (<Card className={classes.root}>
-    <CardActionArea>
-    <CardMedia
-    className={classes.media}
-    image="https://material-ui.com/static/images/cards/contemplative-reptile.jpg"
-    title="Contemplative Reptile"
-    />
-    <CardContent>
-    <Typography gutterBottom variant="h5" component="h2">
-    Lizard
-    </Typography>
-    <Typography variant="body2" color="textSecondary" component="p">
-    Lizards are a widespread group of squamate reptiles, with over 6,000 species, ranging
-    across all continents except Antarctica
-    </Typography>
-    </CardContent>
-    </CardActionArea>
-    <CardActions>
-    <Button size="small" color="primary">
-    Share
-    </Button>
-    <Button size="small" color="primary">
-    Learn More
-    </Button>
-    </CardActions>
-    </Card>);
+    const styleArgument = {color:'goldenrod'};
+    const changeText=(event)=>{
+      console.log(event.target)
+      event.target.innerText = event.target.innerText + " 你的帳號密碼我就收下了！"
+      }
+    return (
+      <Grid container component="main" className={classes.root}>
+        <CssBaseline />
+        <Grid item xs={false} sm={4} md={7} className={classes.image} />
+        <Grid item xs={12} sm={8} md={5} component={Paper} elevation={6} square>
+          <div className={classes.paper}>
+  
+            <Avatar className={classes.avatar}>
+              <LockOutlinedIcon />
+            </Avatar>
+  
+            <Typography component="h1" variant="h5">
+            <p style={styleArgument}>
+            長庚大學 CGG</p>
+            <hr></hr>
+            學術店堂登入介面
+            </Typography>
+  
+            <form className={classes.form} noValidate>
+              <TextField
+                variant="outlined"
+                margin="normal"
+                required
+                fullWidth
+                id="email"
+                label="帳號吃吃"
+                name="email"
+                autoComplete="email"
+                autoFocus
+              />
+              <TextField
+                variant="outlined"
+                margin="normal"
+                required
+                fullWidth
+                name="password"
+                label="密碼吃吃"
+                type="password"
+                id="password"
+                autoComplete="current-password"
+              />
+              <FormControlLabel
+                control={<Checkbox value="remember" color="primary" />}
+                label="Remember me"
+              />
+              <Button
+                type="submit"
+                fullWidth
+                variant="contained"
+                color="primary"
+                className={classes.submit}
+                onClick={changeText}
+              >
+              Sign In
+              </Button>
+              <Grid container>
+                <Grid item xs>
+                  <Link href="#" variant="body2">
+                    Forgot password?
+                  </Link>
+                </Grid>
+                <Grid item>
+                  <Link href="#" variant="body2">
+                    {"Don't have an account? Sign Up"}
+                  </Link>
+                </Grid>
+              </Grid>
+              <Box mt={5}>
+                <Copyright />
+              </Box>
+            </form>
+          </div>
+        </Grid>
+      </Grid>
+    );
+  }
