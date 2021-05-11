590ffc768aefd03896189d4fb550ee472974bf0f
b0829065 <b0829065@cgu.edu.tw>
2021-05-11 21:42:09 +0800

    fix

--- src/App.js
+++ src/App.js
@@ -4,14 +4,16 @@ import UserGithub from './profile.js'
 import SignInSide from './sign_in.js'
 function App() {
   return (
-    // <div className="App">
-    // {SignInSide()}
-    // </div>
     <div className="App">
       <div>
-        {<UserGithub/>}
+        {SignInSide()}
       </div>
     </div>
+    // <div className="App">
+    //   <div>
+    //     {<UserGithub/>}
+    //   </div>
+    // </div>
 
   );
 }
